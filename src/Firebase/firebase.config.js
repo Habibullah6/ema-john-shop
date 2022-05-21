38ea302deb422db805be825785aaabd9ac4091ba
Habibullah6 <nomanhabibullah65@gmail.com>
2022-05-21 21:16:19 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB-dMeUOjgGjY3qW1bUrlshc9HQzpJxkBs",
+    authDomain: "ema-john-simple-ec3b5.firebaseapp.com",
+    projectId: "ema-john-simple-ec3b5",
+    storageBucket: "ema-john-simple-ec3b5.appspot.com",
+    messagingSenderId: "597536110020",
+    appId: "1:597536110020:web:db02a8a67ab589f471a091"
+};
+
+export default firebaseConfig;
